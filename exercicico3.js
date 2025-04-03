31b58576feb1c7d7af7547467329a8332b25d702
Murilow <milanmurilo7@gmail.com>
2025-04-03 18:48:10 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ exercicico3.js
@@ -0,0 +1,9 @@
+let idade = 19
+if (12>idade){
+    console.log('criança')
+}else if(idade>13 && idade<17){
+    console.log('adolescente')
+}else if(idade>18 && idade<59){
+    console.log('adulto')
+}else
+    console.log('idoso')
