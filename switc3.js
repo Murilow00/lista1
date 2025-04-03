9fecf23afa172dc4990ab2fd9e53a0835f4f395e
Murilow <milanmurilo7@gmail.com>
2025-04-03 20:36:06 -0300

    feat(switch-case): adiciona calculadora básica com operações

---
+++ switc3.js
@@ -0,0 +1,19 @@
+let num1 = 3
+let num2 = 8
+let conta = '+'
+switch(conta){
+    case '+':
+        console.log(num1+num2)
+            break
+    case '-':
+        console.log(num1-num2)
+            break
+    case '*':
+        console.log(num1*num2)
+            break
+    case '/':
+        console.log(num1/num2)
+            break
+    default:
+        console.log('insira um operador valido')
+}
